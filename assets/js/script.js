e398c042db1abb1ac2565e06f90b9692281ae98d
Diarra Amadou <72474940+Amaboss10@users.noreply.github.com>
2022-10-21 16:10:22 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-$(document).ready(function () {
-  var $jours = $(".jour");
-  var $puces = $(".bullets .entypo-record		");
-
-  function init() {
-    setTimeout(function () {
-      $("body").addClass("isok");
-      $jours.hide();
-      $(".wrapper").fadeIn("slow", function () {
-        $jours.first().fadeIn("slow");
-        $puces.removeClass("active").first.addClass("active");
-      });
-    }, 2000);
-  }
-  $puce.click(function () {
-    var $this = $(this);
-    var cible = $this.attr("data-cible");
-    $jours.hide();
-
-    $($jours.get(cible)).fadeIn();
-    $puces.removeClass("active");
-    $this.addClass("active");
-  });
-  init();
-});
-=======
 $(document).ready(function(){
 var $jours = $('.jour');var $puces = $('.bullets .entypo-record		');
 
@@ -47,4 +20,3 @@ var $jours = $('.jour');var $puces = $('.bullets .entypo-record		');
 	});
 	init(); 
 );  
->>>>>>> Stashed changes
