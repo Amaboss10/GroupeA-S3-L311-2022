c3b39758cb33fbd4dab669fb3ce29bc4f033c150
Diarra Amadou <amadiarra01@gmail.com>
2022-10-21 16:57:19 +0200

    push final

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,34 @@ var $jours = $('.jour');var $puces = $('.bullets .entypo-record		');
 		}, 2000);
 		
 	}$puce.click(function(){
-		var $this = $(this);var cible = $this.attr('data-cible');
+		var $this =document.addEventListener("DOMContentLoaded", function () {
+			$(document).ready(function () {
+			  var $jours = $(".jour");
+			  var $puces = $(".bullets .entypo-record		");
+		  
+			  function init() {
+				setTimeout(function () {
+				  $("body").addClass("isok");
+				  $jours.hide();
+				  $(".wrapper").fadeIn("slow", function () {
+					$jours.first().fadeIn("slow");
+					$puces.removeClass("active").first().addClass("active");
+				  });
+				}, 2000);
+			  }
+			  $puces.click(function () {
+				var $this = $(this);
+				var cible = $this.attr("data-cible");
+				$jours.hide();
+		  
+				$($jours.get(cible)).fadeIn();
+				$puces.removeClass("active");
+				$this.addClass("active");
+			  });
+			  init();
+			});
+		  });
+		   $(this);var cible = $this.attr('data-cible');
 				$jours.hide();
 
 		$($jours.get(cible)).fadeIn()
